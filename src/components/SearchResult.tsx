11434078ab982b24af28f826d1be2faed25d0b03
meanzzi <meanzi.kim@gmail.com>
2025-05-21 23:59:44 +0900

    feat : result 페이지 퍼블리싱

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -13,44 +13,47 @@ const SearchResult = ({ results }: SearchResultsProps) => {
   }
 
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem] divide-y divide-[#ECECEC]">
       {results.map((item, index) => (
-        <div key={index} className="my-[2rem] flex gap-[1rem]">
+        <div key={index} className="py-[2rem] flex gap-[1rem]">
           <img
             src={item.thumbnail_url}
             alt={item.name}
-            className="w-[18rem] h-[19rem] object-cover rounded-[1rem]"
+            className="w-[17rem] h-[19rem] object-cover rounded-[1rem]"
           />
-
-          <div className="py-2">
-            <div className="text-[1.7rem] font-semibold pb-[1rem] text-[#313131]">
-              {item.name}
-            </div>
-
-            <div className="flex items-center gap-5 text-[1.2rem] pb-[1rem]">
-              <div className="flex">
-                <FaMapMarkerAlt className="text-[#666666]" />
-                <span className="text-[#505050]">{item.address}</span>
+          <div className="flex flex-col justify-between py-2 h-[19rem]">
+            <div>
+              <div className="text-[1.5rem] font-semibold pb-[1rem] text-[#313131]">
+                {item.name}
               </div>
-              <div className="flex">
-                <IoPersonSharp className="text-[#666666]" />
-                <span className="text-[#505050]">
-                  최대 {item.guest_count}인
-                </span>
+
+              <div className="flex items-center text-[1.2rem] pb-[1rem] gap-5">
+                <div className="flex gap-[0.4rem] items-center">
+                  <FaMapMarkerAlt size={14} className="text-[#666666]" />
+                  <span className="text-[#505050]">{item.address}</span>
+                </div>
+                <div className="flex gap-[0.4rem] items-center">
+                  <IoPersonSharp size={14} className="text-[#666666]" />
+                  <span className="text-[#505050]">
+                    최대 {item.guest_count}인
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex text-[1.2rem] pb-[0.8rem] gap-[0.4rem] items-center">
+                <GoStarFill className="text-[#FFCC00]" />
+                <span className="font-semibold">{item.rating}</span>
+              </div>
+
+              <div className="flex gap-3">
+                <div>{item.category}</div>
+                <div>{item.use}</div>
               </div>
             </div>
 
-            <div className="flex text-[1.2rem]">
-              <GoStarFill className="text-[#FFCC00]" />
-              <span className="font-semibold">{item.rating}</span>
+            <div className="font-black text-[#313131] text-[1.7rem] text-right">
+              {item.price_per_hour} 원/시간
             </div>
-
-            <div className="flex">
-              <div>{item.category}</div>
-              <div>{item.use}</div>
-            </div>
-
-            <div>{item.price_per_hour} 원/시간</div>
           </div>
         </div>
       ))}
